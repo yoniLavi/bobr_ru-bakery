b2cab7349507488db3ccdaab9f27f1a6c66523e9
Vladimir Rychkov <vladimir.rychkov@fijowave.com>
2021-10-03 20:02:34 +0100

    cookiecutter-django

---
+++ bakery/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bakery.users.models import User
+from bakery.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
